e03bc2015f5cd4c8b97d65127e4d3b6ee654e68b
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 19:37:02 -0700

    test

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 
 inputvalues = input('Enter all elements values: ')
-numbers1 = inputvalues.split()
-for i in range(len(numbers1)):
-	numbers1[i] = int(numbers1[i]) 
+numbers = inputvalues.split()
+for i in range(len(numbers)):
+	numbers[i] = int(numbers[i]) 
 evenlist = []
 
 i=0
-while numbers1 and i < len(numbers1):
-	if int(numbers1[i]) % 2 == 0:
-		evenlist.append(numbers1.pop(i))
-		i-=1
+while numbers and i < len(numbers):
+	evenlist.append(numbers.pop(i))
 	i+=1
 
-print(numbers1)
-print(evenlist)
+print("The list numbers ", numbers)
+print("The list for even index elements", evenlist)
